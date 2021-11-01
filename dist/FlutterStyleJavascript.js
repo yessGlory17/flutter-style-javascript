270c4386c1d4bb24ae46957461c82e0fd8eeb158
yessGlory17 <ozgurkurucan337@gmail.com>
2021-11-01 21:49:42 +0300

    fix

--- dist/FlutterStyleJavascript.js
+++ dist/FlutterStyleJavascript.js
@@ -1,8 +1,8 @@
-import { Text } from './components/Text';
-import { Button } from './components/Button';
-import { Style } from './components/Style';
-import { Div } from './components/Div';
-import TextTypes from './constant/TextTypes';
-import DisplayTypes from './constant/DisplayTypes';
-import FlexDirectionTypes from './constant/FlexDirectionTypes';
+import { Text } from './components/Text.js';
+import { Button } from './components/Button.js';
+import { Style } from './components/Style.js';
+import { Div } from './components/Div.js';
+import TextTypes from './constant/TextTypes.js';
+import DisplayTypes from './constant/DisplayTypes.js';
+import FlexDirectionTypes from './constant/FlexDirectionTypes.js';
 export { Text, TextTypes, Button, Style, Div, DisplayTypes, FlexDirectionTypes };
